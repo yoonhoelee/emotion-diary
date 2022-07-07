2574392e403867d6034691b08d70512262dfde81
yoonhoelee <ubrain0624@gmail.com>
2022-07-07 16:33:58 +0900

    feat : added home page

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,15 +1,77 @@
 import {useState} from "react";
-
+import {useNavigate} from "react-router-dom";
+import MyButton from "./MyButton";
+import DiaryItem from "./DiaryItem";
+const sortOptionList = [
+    {value: "latest", name: "최신 순"},
+    {value: "oldest", name: "오래된 순"},
+];
+const filterOptionList = [
+    {value: "all", name:"전부 다"},
+    {value: "good", name:"좋은 감정만"},
+    {value: "bad", name: "안 좋은 감정만"}
+];
 const ControlMenu = ({value, onChange, optionList}) => {
-    return <select></select>
+    return (
+        <select className="ControlMenu" value={value} onChange={(e)=>onChange(e.target.value)}>
+        {optionList.map((it, idx) => (
+            <option key={idx} value={it.value}>
+                {it.name}
+            </option>
+        ))}
+    </select>);
 }
 const DiaryList = ({diaryList}) =>{
+    const navigate = useNavigate();
     const [sortType, setSortType] = useState('latest');
+    const[filter,setFilter] = useState("all");
 
-    return (<div>
-        {diaryList.map((it)=><div key={it.id}>{it.content}</div>)}
-    </div>)
-}
+    const getProcessedDiaryList = () =>{
+        const filterCallBack = (item)=>{
+            if(filter==='good'){
+                return parseInt(item.emotion) <=3;
+            }else{
+                return parseInt(item.emotion)>3;
+            }
+        }
+        const compare = (a,b) =>{
+            if(sortType==='latest'){
+                return parseInt(b.date) - parseInt(a.date);
+            }else{
+                return parseInt(a.date) - parseInt(b.date);
+            }
+        }
+        // 원본 배열을 건들이지 않기 위해서 스트링으로 복사하는 과정
+        const copyList = JSON.parse(JSON.stringify(diaryList))
+
+        const filteredList = filter === 'all' ? copyList : copyList.filter((it)=> filterCallBack(it));
+        const sortedList = filteredList.sort(compare)
+        return sortedList;
+    }
+    return (<div className="DiaryList">
+        <div className="menu_wrapper">
+            <div className="left_col">
+                <ControlMenu
+                    value={sortType}
+                    onChange={setSortType}
+                    optionList={sortOptionList}
+                />
+                <ControlMenu
+                    value={filter}
+                    onChange={setFilter}
+                    optionList={filterOptionList}
+                />
+            </div>
+            <div className="right_col">
+                <MyButton type={'positive'} text={"새 일기 쓰기"} onClick={()=>navigate('/new')}/>
+            </div>
+        </div>
+
+        {getProcessedDiaryList().map((it)=>(
+            <DiaryItem key={it.id} {...it}/>
+        ))}
+    </div>);
+};
 DiaryList.defaultProps={
     diaryList: [],
 }
--- src/pages/New.js
+++ src/pages/New.js
@@ -1,7 +1,21 @@
+import {useNavigate} from "react-router-dom";
+import MyHeader from "../components/MyHeader";
+import MyButton from "../components/MyButton";
+
 const New = () =>{
+    const navigate = useNavigate();
     return <div>
-        <h1>New</h1>
-        <p>이 곳은 일기 작성 페이지 입니다</p>
+        <MyHeader headText={"새 일기 쓰기"}
+                  leftChild={<MyButton text={'< 뒤로 가기'} onClick={() => navigate(-1)}/>}
+        />
+        <div>
+            <section>
+                <h4>오늘은 언제인가요?</h4>
+                <div className="input-box">
+
+                </div>
+            </section>
+        </div>
     </div>
 }
 export default New;
